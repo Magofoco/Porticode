19d77b9021c56d388d522c24f41589bac2029d8b
Francesco Stefani <francesco.stefani@outlook.com>
2018-11-17 18:10:41 +0000

    logic welcome page

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('create', views.create, name="create"),
+    path('welcome', views.welcome, name="welcome"),
+    path('home', views.home, name="home"),
+    path('createfreebook', views.createfreebook, name="createfreebook"),
+    path('createexchangebook', views.createexchangebook, name="createexchangebook"),
+
     path("<int:book_id>", views.detail, name="detail"),
 ]
