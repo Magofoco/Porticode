df4f52cf90928dc16c367ccb7d1f846f0127cca5
Michal-lis <michallis95@vp.pl>
2018-11-18 00:42:51 +0000

    little help on frontend

---
+++ utils/populate_database.py
@@ -0,0 +1,8 @@
+from books.models import Book, User
+from books.constants import EXCHANGE_CHOICES, CONDITION_CHOICES, STATUS_CHOICES, OPEN
+from books.constants import GENDER_CHOICES, PREFERENCE_CHOICES
+
+"1"	\
+"pbkdf2_sha256$120000$J33We2DgC5el$EYwL/Um/3PQvFofzzn/Dq68fAp+V3OK5jkn0BKkJjMg="	\
+"2018-11-17 23:51:54.511222"	\
+"0"	"den"	""	""	"0"	"1"	"2018-11-17 23:51:54.351374"	""
